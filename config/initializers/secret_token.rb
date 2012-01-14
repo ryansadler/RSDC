4f45379e171ffd52799f04934c2973ce3b07ea0f
Philip Broadway <philip@abovelabs.com>
2012-01-13 21:16:47 -0800

    init sadler portfolio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ryansadler::Application.config.secret_token = '007a506aa7e957acf97e2f5468fd503d3e26fff90564701ccc60cfe9e8c51ea1f45e7e0849a26bec3bc88dbb02e0f2a4bcf417cf27c3583cd1d292d88bce7c87'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Ryansadler::Application.routes.draw do
+  get "home/index"
+  root :to => 'home#index'
+end
